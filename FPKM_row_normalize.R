812804a53f8156c3af2921736276bd483c87a9c3
Timothy Laurent <timothyjlaurent@gmail.com>
2013-06-10 15:11:56 -0700

    Script to make boxplot of row normalized FPKM values from USEQ output

--- FPKM_row_normalize.R
+++ FPKM_row_normalize.R
@@ -1,3 +1,11 @@
+install.packages("som")
+library("som")
 useqout.77.table <- read.delim("~/BioinfCore/useqout-77-table.txt")
 
 FPKMtab <- useqout.77.table[,grep('FPKM_', names(useqout.77.table))]
+
+colnames(FPKMtabNorm) <-  colnames(FPKMtab)
+
+experimentName = 77
+
+boxplot(FPKMtabNorm, use.cols=TRUE, ylab='normalized FPKM values', par(las=3, mar=c(20,4,4,2)))
